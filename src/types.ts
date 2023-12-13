784dfeb65a100e632966de911d63fe6c706b83c9
karinadarc <darcsilva.kd@gmail.com>
2023-12-13 16:46:01 -0300

    POST task implementado

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,14 @@
-export type TUserDB ={
+export type TUserDB = {
     id: string,
     name: string,
     email: string,
     password: string
+}
+
+export type TTaskDB = {
+    id: string,
+    title: string,
+    description: string,
+    created_at: string,
+    status: number
 }
